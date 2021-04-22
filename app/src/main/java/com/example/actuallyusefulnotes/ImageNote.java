635f8dfff270fe982beabed9f5960cbfb74446d2
Luca-Di-Croce <61190535+Luca-Di-Croce@users.noreply.github.com>
2021-04-22 17:23:24 +0200

    Delete ImageNote.java

--- app/src/main/java/com/example/actuallyusefulnotes/ImageNote.java
+++
@@ -1,7 +0,0 @@
-package com.example.actuallyusefulnotes;
-
-public class ImageNote extends BasicNote{
-    public ImageNote(String titulo, String categoria, String date, String time, String author){
-        super(titulo, categoria, date, time, author);
-    }
-}
