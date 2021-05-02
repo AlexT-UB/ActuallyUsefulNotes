ea61b72930fe8ffbe074cd8aa067811a9a029513
mruizavi48 <mruizavi48@alumnes.ub.edu>
2021-05-02 12:34:00 +0200

    avance lógica diseño

--- app/src/main/java/com/example/actuallyusefulnotes/AddNote.java
+++ app/src/main/java/com/example/actuallyusefulnotes/AddNote.java
@@ -3,17 +3,29 @@ package com.example.actuallyusefulnotes;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class AddNote extends AppCompatActivity {
-    private Toolbar toolbar;
+    FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_note);
+        setContentView(R.layout.activity_main);
 
-        toolbar = findViewById(R.id.topAppBar);
-        setSupportActionBar(toolbar);
+        floatingActionButton = findViewById(R.id.addNote);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent opciones = new Intent(AddNote.this, ElegirTipoNota.class);
+                startActivity(opciones);
+            }
+        });
+
     }
 }
